30bceef462cd43c55f0c44c6e3da057a036a71ae
Vəzirxan Tarverdiyev <vezir.tarverdi@icloud.com>
2018-02-15 20:45:26 +0400

    Add files via upload

---
+++ 14.02.2018/14.02.2018/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace _14._02._2018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tesdiqbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.secilmiwLbl = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // tesdiqbtn
+            // 
+            this.tesdiqbtn.Location = new System.Drawing.Point(46, 308);
+            this.tesdiqbtn.Name = "tesdiqbtn";
+            this.tesdiqbtn.Size = new System.Drawing.Size(75, 23);
+            this.tesdiqbtn.TabIndex = 0;
+            this.tesdiqbtn.Text = "Tesdiqle";
+            this.tesdiqbtn.UseVisualStyleBackColor = true;
+            this.tesdiqbtn.Click += new System.EventHandler(this.tesdiqbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Onceden Baxiw";
+            // 
+            // secilmiwLbl
+            // 
+            this.secilmiwLbl.AutoSize = true;
+            this.secilmiwLbl.Location = new System.Drawing.Point(49, 225);
+            this.secilmiwLbl.Name = "secilmiwLbl";
+            this.secilmiwLbl.Size = new System.Drawing.Size(0, 13);
+            this.secilmiwLbl.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(563, 42);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(202, 186);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 370);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.secilmiwLbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tesdiqbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tesdiqbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label secilmiwLbl;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 14.02.2018/14.02.2018/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14._02._2018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Button> btns = new List<Button>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                for (int j = 1; j <= 8; j++)
+                {
+                    Button btn = new Button();
+                    btn.Top = i * 40 + 42;
+                    btn.Left = (j-1) * 40 + 42;
+                    btn.Width = 40;
+                    btn.Height = 40;
+                    btn.BackColor = Color.White;
+
+                    btn.Text = i * 8 + j+"";
+
+
+                    // buttona click eventi elave edilir
+                    btn.Click += new EventHandler(btn_Click);
+
+
+                    btns.Add(btn);
+                    this.Controls.Add(btn);
+                }
+
+                Label lbl = new Label();
+
+                lbl.Text = i * 5 +5+" azn";
+                lbl.Left = 380;
+                lbl.Top = i * 40 + 52;
+                this.Controls.Add(lbl);
+
+            }
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            if(btn.BackColor == Color.Yellow)
+            {
+                btn.BackColor = Color.White;
+            }
+            else
+            {
+                btn.BackColor = Color.Yellow;
+            }
+            detect_places();
+        }
+
+        string s = "";
+        int n = 0;
+        public void detect_places()
+        {
+
+            s = "";
+            n = 0;
+            foreach (var b in btns)
+            {
+                if (b.BackColor == Color.Yellow)
+                {
+                    s += b.Text + ",";
+                    n += (int.Parse(b.Text) / 9 + 1) * 5;
+                }
+
+            }
+
+
+            secilmiwLbl.Text = "Umumi qiyet : " + n + "\n" + "Secdiyiniz yerler : " + s;
+        }
+
+        private void tesdiqbtn_Click(object sender, EventArgs e)
+        {
+            if (n == 0)
+            {
+                MessageBox.Show("Once yer secin");
+            }
+            else
+            {
+                foreach (var item in btns)
+                {
+                    if (item.BackColor == Color.Yellow)
+                    {
+                        item.BackColor = Color.Green;
+                        item.Enabled = false;
+                    }
+                }
+                listBox1.Items.Add("Qiyemt: "+n+ "azn     Alinan yerler : "+s);
+                s = "";
+                n = 0;
+                secilmiwLbl.Text = "Umumi qiyet : " + n + "\n" + "Secdiyiniz yerler : " + s;
+
+            }
+
+        }
+    }
+}
